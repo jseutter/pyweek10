63248d001568cd5bb4d026357957b906f7117b50
Wes Devauld <wes@devauld.ca>
2010-04-02 10:40:39 -0700

    Keep upward force even after the key has been removed

--- lib/jornithopter_game.py
+++ lib/jornithopter_game.py
@@ -51,7 +51,7 @@ class GameMode(mode.Mode):
         elif sym == key.RIGHT:
             self.dir = self.dir & ~RIGHT
         elif sym == key.UP:
-            self.dir = self.dir & ~UP
+            # We want to keep going up on a stroke self.dir = self.dir & ~UP
         else:
             return EVENT_UNHANDLED
         return EVENT_HANDLED
